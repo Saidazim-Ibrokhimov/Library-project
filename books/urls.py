4e107040d5b53d66b2c04f094b3600f5478a9f8d
Saidazim Ibrokhimov <saidazimibroximov04@gmail.com>
2023-10-13 18:07:03 +0500

    Updated urls.py file

--- books/urls.py
+++ books/urls.py
@@ -15,11 +15,11 @@ router.register('books', BookViewSet, basename='books')
 
 urlpatterns = [
     path('', BookListApiView.as_view()),
-    path('delete/<int:pk>/', BookDeleteApiView.as_view()),
-    path('update/<int:pk>/', BookUpdateApiView.as_view()),
     path('detail/<int:pk>/', BookDetailApiView.as_view()),
+    path('update/<int:pk>/', BookUpdateApiView.as_view()),
+    path('delete/<int:pk>/', BookDeleteApiView.as_view()),
     path('create/', BookCreateApiView.as_view()),
-    path('update-delete/<int:pk>/', BookUpdateDeleteApiView.as_view()),
+ 
     ]
 
 urlpatterns += router.urls
